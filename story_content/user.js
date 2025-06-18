b0cad14800ff5dc2ab03c71f52cec6fabb8584a8
vickyexl <vickyvrnd.exl@gmail.com>
2025-06-18 17:30:33 +0700

    Utama

---
+++ story_content/user.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6GxjcHCNKb1":
+        Script1();
+        break;
+      case "6CMAUzi3ME2":
+        Script2();
+        break;
+      case "6nNZiV52HsR":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  localStorage.clear();
+sessionStorage.clear();
+window.location.href = "https://www.google.com/";
+}
+
+function Script2()
+{
+  localStorage.clear();
+sessionStorage.clear();
+window.location.href = "https://www.google.com/";
+}
+
+function Script3()
+{
+  localStorage.clear();
+sessionStorage.clear();
+window.location.href = "https://www.google.com/";
+}
+
